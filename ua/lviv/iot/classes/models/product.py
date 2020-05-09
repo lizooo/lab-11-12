8bab67c2b4bbdbc9c57ae35f21f568adbccf3513
Liza <lizadudyk@gmail.com>
2020-05-09 16:22:30 +0300

    Edited __init__

--- ua/lviv/iot/classes/models/product.py
+++ ua/lviv/iot/classes/models/product.py
@@ -1,10 +1,9 @@
-import datetime
+
 from ua.lviv.iot.classes.models.agegroup import AgeGroup
 
 
 class Product:
-    def __init__(self, age_group=AgeGroup.ONE_TWO_MONTH, price_in_uah = 0.0, producer ="not specified", shelf_life_in_days = 0):
-        self.age_group = age_group
+    def __init__(self, price_in_uah=0.0, producer="not specified", shelf_life_in_days = 0):
         self.price_in_uah = price_in_uah
         self.producer = producer
         self.shelf_life_in_days = shelf_life_in_days
